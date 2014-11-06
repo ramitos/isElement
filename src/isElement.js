9f958486a37fd8825fbbaea0e65a67faf8e8a47e
Sérgio Ramos <mail@sergioramos.me>
2014-11-06 13:51:36 +0000

    style

--- src/isElement.js
+++ src/isElement.js
@@ -1,10 +1,7 @@
-var type = require('type')
+var type = require('type');
 
-module.exports = function (el) {
-  var element = HTMLElement
-    ? HTMLElement
-    : Element
-
-  if(type(element) === 'function') return (el instanceof element)
-  return type(el) === 'element' && type(el.nodeName) === 'string'
-}
+module.exports = function(el) {
+  var element = HTMLElement ? HTMLElement : Element;
+  if (type(element) === 'function') return (el instanceof element);
+  return type(el) === 'element' && type(el.nodeName) === 'string';
+};
